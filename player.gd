b0acd6f033bd04a3de49a65cbde1c26f7998048f
Level-Up-Gaming <info@levelupgaming.party>
2025-10-04 08:56:51 +0100

    ff

--- player.gd
+++ player.gd
@@ -26,10 +26,10 @@ func _physics_process(delta):
 	camera_pivot.global_position = camera_pivot.global_position.lerp(global_position, delta * 20)
 
 	# Smoothly interpolate the camera pivot's transformation (position, rotation, scale) towards the vehicle body's transformation
-	camera_pivot.transform = camera_pivot.transform.interpolate_with(transform, delta * 5)
+	#camera_pivot.transform = camera_pivot.transform.interpolate_with(transform, delta * 5)
 
 	
-	look_at = look_at.lerp(global_position + linear_velocity, delta * 5.0)
+	#look_at = look_at.lerp(global_position + linear_velocity, delta * 5.0)
 	camera_3d.look_at(look_at)
 	#reverse_camera.look_at(look_at)
 	#_check_camera_switch()
